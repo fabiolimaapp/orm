dac18f60478dfe886da9b11af1e91602527ba68c
fabiolimaapp <helitonfabio@gmail.com>
2026-04-19 17:50:25 -0300

    Add files via upload

---
+++ orm_pwa_package/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'orm-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://fonts.googleapis.com/css2?family=Cinzel:wght@400;600;700&family=Crimson+Pro:ital,wght@0,300;0,400;0,600&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
